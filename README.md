a746ba827191d7cc8749b1ff21a270ff664d7e9f
Ben Boarder <ben@reactordigital.com.au>
2015-12-03 11:18:46 +1100

    first draft

--- README.md
+++ README.md
@@ -1,37 +1,19 @@
-Scotch Box
+React-Box
 ==========
 
+## A fork of scotch-box from [scotch.io](http://scotch.io) but with a few modifications
 ## Check out the official docs at: [box.scotch.io][16]
-## [Read the getting started article](https://scotch.io/bar-talk/introducing-scotch-box-a-vagrant-lamp-stack-that-just-works)
-## [Read the 2.0 release article](https://scotch.io/bar-talk/announcing-scotch-box-2-0-our-dead-simple-vagrant-lamp-stack-improved)
-## [Read the 2.5 release article](https://scotch.io/bar-talk/announcing-scotch-box-2-5)
 
-![Scotch Box](https://scotch.io/wp-content/uploads/2015/11/scotch-box-2.5-share.png)
+Nothing crazy here, just a few mods I use on nearly every project; so this makes it easier than having to set it up time and time again.
 
-Scotch Box is a preconfigured Vagrant Box with a full array of LAMP Stack features to get you up and running with Vagrant in no time.
+### React-Box Extras
 
-A lot of PHP websites and applications don’t require much server configuration or overhead at first. This box should have all your needs for doing basic development so you don’t have to worry about configuring Vagrant and you can simply focus on your code.
-
-No provisioning tools or setup is really even required with Scotch Box. Since everything is packaged into the box, running “vagrant” is super fast, you’ll never have to worry about your environment breaking with updates, and you won’t need Internet to code.
-
-
-![Scotch Box](https://scotch.io/wp-content/uploads/2015/11/Screen-Shot-2015-11-03-at-1.31.52-PM.png)
-
-
-## What and Why
-
-Vagrant is an extremely powerful tool. With Chef or Puppet and Vagrant, you can configure any type of server environment you can think of. The possibilities are endless (especially with Docker in the picture now, too). Speaking candidly though, most the development I do doesn't really stray from a default LAMP stack, and when I have to configure a server, I really am always just setting up a boring typical LAMP stack anyways. **All I really want is PHP 5.6 and a bunch of modules with zero hassle or overhead**.
-
-I used to use this seriously awesome [Vagrant LAMP Stack][1] that I even wrote about [here][2]. The problem with this is it broke a lot. It broke when Vagrant updated, it broke when Chef updated, and it broke when Berkshelf updated. On top of that, I always had problems getting it working on Windows. There are just too many points of failures for what it's purpose was for me - simply just developing locally.
-
-So that's why I decided to build a Vagrant LAMP Box. The box is prepackaged and requires provisioning and no configuration. You simply boot it up and it just works. **It's not for every project, but it sure will help you get straight to it with a lot of them**.
-
-
-> Are you new to Vagrant? If your new to Vagrant, check out our [getting started guide with Vagrant][2] article, our [Vagrant Share article][10], and our article on [Larvel’s Vagrant stack Homestead][11]. If you follow the first tutorial, you can just learn the Vagrant commands but use the Scotch Box instead.
-
-
-![Scotch Box SSH](https://cask.scotch.io/2014/10/scotch-box-ssh.jpg)
-
+- tree
+- htop
+- NVM (Node Version Manager)
+    + Node 0.12.4
+- RVM (Ruby Version Manager)
+    + Ruby 2.2.1
 
 ## Features
 
@@ -126,7 +108,7 @@ vagrant ssh
 
 ## Database Access
 
-### MySQL 
+### MySQL
 
 - Hostname: localhost or 127.0.0.1
 - Username: root
@@ -213,23 +195,3 @@ Permission is hereby granted, free of charge, to any person obtaining a copy of 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-
-
- [1]: https://github.com/MiniCodeMonkey/Vagrant-LAMP-Stack
- [2]: http://scotch.io/tutorials/get-vagrant-up-and-running-in-no-time
- [3]: https://www.vagrantup.com/downloads.html
- [4]: https://www.virtualbox.org/wiki/Downloads
- [5]: http://www.sequelpro.com/
- [6]: http://www.navicat.com/
- [7]: http://github.com/scotch-io
- [8]: http://twitter.com/scotch_io
- [9]: https://github.com/smdahlen/vagrant-hostmanager
- [10]: http://scotch.io/tutorials/sharing-your-virtual-machine-on-the-web-with-vagrant-share
- [11]: http://scotch.io/tutorials/php/getting-started-with-laravel-homestead
- [12]: https://www.vagrantup.com/downloads.html
- [13]: https://www.virtualbox.org/wiki/Downloads
- [14]: http://192.168.33.10/
- [15]: https://github.com/smdahlen/vagrant-hostmanager
- [16]: http://box.scotch.io
- [17]: http://scotch.io/bar-talk/introducing-scotch-box-a-vagrant-lamp-stack-that-just-works
--- public/index.php
+++ public/index.php
@@ -9,36 +9,33 @@
     <!-- Meta -->
     <meta charset="utf-8">
     <!--
-       d888888o.       ,o888888o.        ,o888888o. 8888888 8888888888 ,o888888o.    8 8888        8
-     .`8888:' `88.    8888     `88.   . 8888     `88.     8 8888      8888     `88.  8 8888        8
-     8.`8888.   Y8 ,8 8888       `8. ,8 8888       `8b    8 8888   ,8 8888       `8. 8 8888        8
-     `8.`8888.     88 8888           88 8888        `8b   8 8888   88 8888           8 8888        8
-      `8.`8888.    88 8888           88 8888         88   8 8888   88 8888           8 8888        8
-       `8.`8888.   88 8888           88 8888         88   8 8888   88 8888           8 8888        8
-        `8.`8888.  88 8888           88 8888        ,8P   8 8888   88 8888           8 8888888888888
-    8b   `8.`8888. `8 8888       .8' `8 8888       ,8P    8 8888   `8 8888       .8' 8 8888        8
-    `8b.  ;8.`8888    8888     ,88'   ` 8888     ,88'     8 8888      8888     ,88'  8 8888        8
-     `Y8888P ,88P'     `8888888P'        `8888888P'       8 8888       `8888888P'    8 8888        8
+         ,ggggggggggg,      ,ggggggg,            ,ggg,       ,gggg,   ,ggggggggggggggg
+    dP"""88""""""Y8,  ,dP""""""Y8b          dP""8I     ,88"""Y8b,dP""""""88"""""""
+    Yb,  88      `8b  d8'    a  Y8         dP   88    d8"     `Y8Yb,_    88
+     `"  88      ,8P  88     "Y8P'        dP    88   d8'   8b  d8 `""    88
+         88aaaad8P"   `8baaaa            ,8'    88  ,8I    "Y88P'        88
+         88""""Yb,   ,d8P""""            d88888888  I8'                  88
+         88     "8b  d8"           __   ,8"     88  d8                   88
+         88      `8i Y8,          dP"  ,8P      Y8  Y8,            gg,   88
+         88       Yb,`Yba,,_____, Yb,_,dP       `8b,`Yba,,_____,    "Yb,,8P
+         88        Y8  `"Y8888888  "Y8P"         `Y8  `"Y8888888      "Y8P'
 
-    8 888888888o       ,o888888o.  `8.`8888.      ,8'
-    8 8888    `88.  . 8888     `88. `8.`8888.    ,8'
-    8 8888     `88 ,8 8888       `8b `8.`8888.  ,8'
-    8 8888     ,88 88 8888        `8b `8.`8888.,8'
-    8 8888.   ,88' 88 8888         88  `8.`88888'
-    8 8888888888   88 8888         88  .88.`8888.
-    8 8888    `88. 88 8888        ,8P .8'`8.`8888.
-    8 8888      88 `8 8888       ,8P .8'  `8.`8888.
-    8 8888    ,88'  ` 8888     ,88' .8'    `8.`8888.
-    8 888888888P       `8888888P'  .8'      `8.`8888.
+        8 888888888o       ,o888888o.  `8.`8888.      ,8'
+        8 8888    `88.  . 8888     `88. `8.`8888.    ,8'
+        8 8888     `88 ,8 8888       `8b `8.`8888.  ,8'
+        8 8888     ,88 88 8888        `8b `8.`8888.,8'
+        8 8888.   ,88' 88 8888         88  `8.`88888'
+        8 8888888888   88 8888         88  .88.`8888.
+        8 8888    `88. 88 8888        ,8P .8'`8.`8888.
+        8 8888      88 `8 8888       ,8P .8'  `8.`8888.
+        8 8888    ,88'  ` 8888     ,88' .8'    `8.`8888.
+        8 888888888P       `8888888P'  .8'      `8.`8888.
     -->
     <title>Scotch Box ♥ A Vagrant LAMP Stack for Beginners That Just Works</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="yes" name="apple-mobile-web-app-capable">
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
-
-    <!-- Favicons -->
-    <link rel="shortcut icon" sizes="16x16 24x24 32x32 48x48 64x64" href="http://scotch.io/favicon.ico">
 
     <!-- Styles -->
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
@@ -70,35 +67,6 @@
         section {
             position: relative;
         }
-        #topper {
-            margin-top: 51px;
-            height: 450px;
-            text-align: center;
-        }
-        #topper svg {
-            position: absolute;
-            left: 0;
-            top: 0;
-            width: 100%;
-            height: 100%;
-        }
-        #topper h1 {
-            position: absolute;
-            color: #fff;
-            left: 50%;
-            top: 50%;
-            -o-transform: translate(-50%, -50%);
-            -ms-transform: translate(-50%, -50%);
-            -moz-transform: translate(-50%, -50%);
-            -webkit-transform: translate(-50%, -50%);
-            transform: translate(-50%, -50%);
-            margin: 0;
-            font-size: 80px;
-            text-transform: uppercase;
-        }
-        #topper h1 i {
-            color: rgb(255, 157, 157);
-        }
         article {
             padding: 50px 0;
         }
@@ -106,7 +74,7 @@
             background: #e3e3e3;
         }
         article .content .wrap {
-            background: rgba(255, 27, 53, 0.8);
+            background: rgba(78, 78, 78, 0.8);
             margin-bottom: 50px;
             padding-top: 10px;
             padding-bottom: 10px;
@@ -136,40 +104,42 @@
         #site-footer a:hover {
             color: rgb(144, 255, 184);
         }
+        h1 {
+            text-align: center;
+        }
     </style>
 
 </head>
 <body>
 
-    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="/">Scotch Box</a>
-            </div>
-            <div class="collapse navbar-collapse">
-                <ul class="nav navbar-nav pull-right">
-                    <li><a href="http://box.scotch.io">Docs</a></li>
-                    <li><a href="http://github.com/scotch-io/scotch-box">Star on GitHub</a></li>
-                    <li><a href="http://scotch.io">Scotch.io</a></li>
-                    <li><a href="https://twitter.com/scotch_io">@scotch_io</a></li>
-                    <li><a href="https://twitter.com/nickforthought">@nickforthought</a></li>
-                </ul>
-            </div>
-        </div>
-    </div>
-
-    <section id="topper" style="background-image: url('data:image/svg+xml;base64,<?php echo base64_encode('<svg xmlns="http://www.w3.org/2000/svg" version="1.1"><defs id="defs4"><filter color-interpolation-filters="sRGB" id="filter3115"><feTurbulence type="fractalNoise" numOctaves="1" baseFrequency="0.9" id="feTurbulence3117"/><feColorMatrix result="result5" values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 6 -4.15 " id="feColorMatrix3119"/><feComposite in2="result5" operator="in" in="SourceGraphic" result="result6" id="feComposite3121"/><feMorphology in="result6" operator="dilate" radius="1" result="result3" id="feMorphology3123"/></filter></defs><rect width="100%" height="100%" x="0" y="0" id="rect2985" fill="#000000"/><rect width="100%" height="100%" x="0" y="0" id="rect2985" style="fill:#FF4343;filter:url(#filter3115)"/></svg>'); ?>');">
-        <h1>Welcome to<br>Scotch Box 2.5 <i class="fa fa-heart"></i></h1>
-    </section>
+    <h1>React Box</h1>
 
     <article>
         <div class="container">
+
+            <div class="row content">
+                <div class="col-md-6 col-md-offset-3 wrap">
+                    <h2>Extra React Stuff</h2>
+                    <table class="table table-responsive table-striped table-hover">
+                        <tr>
+                            <td>tree</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                        <tr>
+                            <td>htop</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                        <tr>
+                            <td>NVM (Node Version Manager)</td>
+                            <td>Node 0.12.4</td>
+                        </tr>
+                        <tr>
+                            <td>RVM (Ruby Version Manager)</td>
+                            <td>Ruby 2.2.1</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
 
             <div class="row content">
                 <div class="col-md-6 col-md-offset-3 wrap">
